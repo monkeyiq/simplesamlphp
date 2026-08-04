monkeyiq/simplesamlphp

commit 03092b86d0c369159b5edcbcb8256d3340bd1326

Fix HTTP-client when using a proxy (#2622)
* Fix HTTP-client when using a proxy

* Add upgrade note on web-proxies

* Add index for 2.5-release

* Note about env-vars

* Fix bare url

src/SimpleSAML/Utils/HTTP.php
@@ -44,13 +44,12 @@ class HTTP
         $proxy = $config->getOptionalString('proxy', null);
 
         if ($proxy !== null) {
+            $proxy = preg_replace('/^(tcp:\/\/)+/i', 'http://', $proxy);
             $proxyAuth = $config->getOptionalString('proxy.auth', null);
             $scheme = parse_url($proxy, PHP_URL_SCHEME);
 
             if ($proxyAuth !== null) {
                 $proxy = ['proxy' => str_replace($scheme . '://', $scheme . '://' . $proxyAuth . '@', $proxy)];
-            } elseif ($proxy !== null) {
-                $proxy = ['proxy' => str_replace($scheme . '://', 'http://', $proxy)];
             }
             $options = array_merge($proxy, $options);
         }

commit 095dce87a7373b4d0700bcd91531116d135b7566

Update HTTP.php Fix proxy (#2662)
* Update HTTP.php

Fix a regression comming from https://github.com/simplesamlphp/simplesamlphp/commit/03092b86d0c369159b5edcbcb8256d3340bd1326

At least make $proxy an array for array_merge when proxyAuth is not available

* Update HTTP.php

Maintainability

src/SimpleSAML/Utils/HTTP.php
@@ -46,11 +46,14 @@ class HTTP
         if ($proxy !== null) {
             $proxy = preg_replace('/^(tcp:\/\/)+/i', 'http://', $proxy);
             $proxyAuth = $config->getOptionalString('proxy.auth', null);
-            $scheme = parse_url($proxy, PHP_URL_SCHEME);
-
+            
             if ($proxyAuth !== null) {
-                $proxy = ['proxy' => str_replace($scheme . '://', $scheme . '://' . $proxyAuth . '@', $proxy)];
+                $scheme = parse_url($proxy, PHP_URL_SCHEME);
+                $proxy = str_replace($scheme . '://', $scheme . '://' . $proxyAuth . '@', $proxy);
             }
+
+            $proxy = ['proxy' => $proxy];
+            
             $options = array_merge($proxy, $options);
         }
 